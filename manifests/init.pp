5c056101c762d290fc1b4dfe3695a3c415b52f19
Benjamin Robertson <ben.robertson@puppet.com>
2021-11-29 23:09:34 +1100

    Update comment

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 # @summary A short summary of the purpose of this class
 #
-# A description of what this class does
+# A description of what this class does update
 #
 # @example
 #   include dog_site
